50c40276671a41b3d75502a5be67c9ed61969cdf
Mohsendream <madiounimohsen@gmail.com>
2023-03-11 23:07:47 +0100

    Versioning Test

--- src/main/scala/com/philcoutinho/Posts.scala
+++ src/main/scala/com/philcoutinho/Posts.scala
@@ -8,7 +8,7 @@ object Posts {
   def posts(df : DataFrame): DataFrame={
     val df_1=df.select(explode(col("GraphImages")).as("GraphImage"))
     val df_2 = df_1.select(col("GraphImage.*")).drop("comments", "display_url","gating_info","location",
-      "media_preview", "thumbnail_resources", "thumbnail_src", "urls")
+      "media_preview", "thumbnail_resources", "thumbnail_src", "urls").withColumn("Id", lit(true))
     df_2
   }
 }
