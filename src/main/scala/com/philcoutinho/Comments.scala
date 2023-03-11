89d80f8b3e4b730e9119d437e36915ba664aeb08
Mohsendream <madiounimohsen@gmail.com>
2023-03-11 22:49:06 +0100

    Versioning Test

--- src/main/scala/com/philcoutinho/Comments.scala
+++ src/main/scala/com/philcoutinho/Comments.scala
@@ -1,21 +1,22 @@
 package com.philcoutinho
+
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, _}
 
 object Comments {
-  def Comments(df:DataFrame): DataFrame= {
-    val df_1 = df.select(explode(col("GraphImages")).as("GraphImage"))
-    val df_2 = df_1.select(col("GraphImage.*"))
-    val df_3 = df_2.select(explode(col("comments.data")).as("Data"),
-      col("username").as("username"))
-    val df_4=df_3.select(col("data.created_at").as("created_at"),
-      col("data.id").as("comment_id"),
-      col("data.text").as("text"),
+
+  def comments(df: DataFrame): DataFrame = {
+    val explodedGraphImage = df.select(explode(col("GraphImages")).as("GraphImage"))
+    val explodedDataArray = explodedGraphImage.select(explode(col("GraphImage.comments.data")).as("Data"),
+      col("GraphImage.username").as("username"))
+    val commentsData = explodedDataArray.select(col("Data.created_at").as("created_at"),
+      col("Data.id").as("comment_id"),
+      col("Data.text").as("text"),
       col("Data.owner.id").as("owner_id"),
       col("Data.owner.username").as("comment_username"),
-     col("username").as("username")
+      col("username").as("username")
     )
-    df_4
+    commentsData
   }
 }
 
--- src/test/scala/com/philcoutinho/CommentsSpec.scala
+++ src/test/scala/com/philcoutinho/CommentsSpec.scala
@@ -1,27 +1,29 @@
 package com.philcoutinho
 
-import com.philcoutinho.Comments.Comments
+import com.philcoutinho.Comments.comments
 import org.apache.spark.sql.SparkSession
 import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-case class Data ( created_at: Long, comment_id : String, text : String, owner_id : String, comment_username : String, username : String )
+case class Data(created_at: Long, comment_id: String, text: String, owner_id: String, comment_username: String, username: String)
+
 class CommentsSpec extends AnyFlatSpec with Matchers with GivenWhenThen {
-  implicit val spark= SparkSession
+  implicit val spark = SparkSession
     .builder()
     .master("local[*]")
     .appName("comment_Test")
     .getOrCreate()
+
   import spark.implicits._
 
   "Comments" should "Flatten the JSON Dataframe" in {
     Given("the JSON Dataframe")
     val df = spark.read.option("multiLine", true).json("C:\\Users\\user\\IdeaProjects\\PhilCoutinho_2\\target\\sample.json")
     When("Comments is invoked")
-    val comments = Comments(df)
+    val result = comments(df)
     Then("the Flattened DataFrame should be returned")
-    val expectedResult =Seq(Data(1619023963L,"18209883163069294","💪🏼💪🏼","20740995","sergiroberto","phil.coutinho")).toDF()
-    expectedResult.as[Data].collect() should contain theSameElementsAs comments.as[Data].collect()
+    val expectedResult = Seq(Data(1619023963L, "18209883163069294", "💪🏼💪🏼", "20740995", "sergiroberto", "phil.coutinho")).toDF()
+    expectedResult.collect() should contain theSameElementsAs result.collect()
   }
 }
