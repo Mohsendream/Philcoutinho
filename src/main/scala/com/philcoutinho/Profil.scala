bdb9dfa873b16bcf206b5d2538ccf95d9697239e
Mohsendream <madiounimohsen@gmail.com>
2023-03-13 10:40:26 +0100

    Versioning Test

--- src/main/scala/com/philcoutinho/Profil.scala
+++ src/main/scala/com/philcoutinho/Profil.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 
 object Profil {
-  def profil(df:DataFrame):DataFrame ={
+  def Profil(df:DataFrame):DataFrame ={
     val df_1=df.select(col("GraphProfileInfo.*"))
     df_1
   }
