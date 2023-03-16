1f5f82a11becf5b05277bada9537ee83b3777034
Mohsendream <madiounimohsen@gmail.com>
2023-03-16 09:52:31 +0100

    grouped the posts by typename and detrmine nbre of comments and how many people comment that post

--- src/main/scala/com/philcoutinho/gold/MostCommentor.scala
+++ src/main/scala/com/philcoutinho/gold/MostCommentor.scala
@@ -9,7 +9,7 @@ object MostCommentor {
   def mostcommentor(CommentData: DataFrame,spark : SparkSession): DataFrame = {
 
     CommentData.createOrReplaceTempView("my_table")
-    val result = spark.sql("SELECT  owner_id,comment_username, COUNT(*) as nbr FROM my_table GROUP BY owner_id,comment_username")
+    val result = spark.sql("SELECT  owner_id,comment_username, COUNT(*) as nbr FROM my_table GROUP BY owner_id,comment_username ORDER BY nbr DESC")
     result
   }
 }
