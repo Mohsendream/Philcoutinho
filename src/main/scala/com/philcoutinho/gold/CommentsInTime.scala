5925105cdb425571caca9557701799dac9312b9b
Mohsendream <madiounimohsen@gmail.com>
2023-03-16 10:14:58 +0100

    grouped the posts by typename and detrmine nbre of comments and how many people comment that post

---
+++ src/main/scala/com/philcoutinho/gold/CommentsInTime.scala
@@ -0,0 +1,16 @@
+package com.philcoutinho.gold
+
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+object CommentsInTime {
+
+  def commentsintime(CommentData : DataFrame, spark : SparkSession):DataFrame={
+
+    CommentData.createOrReplaceTempView("my_table")
+    val result = spark.sql("SELECT __typename, created_at, comment_id, owner_id, comment_username, text " +
+      " FROM my_table WHERE FROM_UNIXTIME (created_at) BETWEEN ('2020-01-01') AND ('2021-05-16')")
+    result
+  }
+}
--- src/test/scala/com/philcoutinho/gold/CommentsInTimeSpec.scala
+++ src/test/scala/com/philcoutinho/gold/CommentsInTimeSpec.scala
@@ -1,31 +1,32 @@
 package com.philcoutinho.gold
 
-
-import com.philcoutinho.gold.MostCommentor.mostcommentor
+import com.philcoutinho.gold.CommentsInTime.commentsintime
 import com.philcoutinho.silver.Comments.comments
 import org.apache.spark.sql.SparkSession
 import org.scalatest.GivenWhenThen
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-case class CommentsData(owner_id: String,  comment_username: String, sum: Long)
+case class TimeData (__typename: String, created_at: Long, comment_id: String, owner_id: String, comment_username: String, text : String)
 
-class MostCommentorSpec extends AnyFlatSpec with Matchers with GivenWhenThen {
+class CommentsInTimeSpec extends AnyFlatSpec with Matchers with GivenWhenThen {
   implicit val spark = SparkSession
     .builder()
     .master("local[*]")
     .appName("comment_Test")
     .getOrCreate()
+
   import spark.implicits._
 
-  "MostCommentor" should "return the person who commented the most" in {
+  "MostCommentintime" should "return the comments in a specific period" in {
     Given("the specified table")
     val InputData = spark.read.option("multiLine", true).json("C:\\Users\\user\\IdeaProjects\\PhilCoutinho_2\\target\\sample.json")
-    val CommentData =comments(InputData)
-    When ("mostcommentor is invoked")
-    val result = mostcommentor(CommentData, spark)
+    val CommentData = comments(InputData)
+    When("commentsInTime is invoked")
+    val result = commentsintime(CommentData,spark)
     Then("the result should be returned")
-    val expectedResult=Seq(CommentsData("20740995", "sergiroberto", 1L)).toDF()
+    val expectedResult = Seq(TimeData("GraphImage", 1619023963, "18209883163069294", "20740995", "sergiroberto","💪🏼💪🏼")).toDF()
     expectedResult.collect() should contain theSameElementsAs result.collect()
   }
 }
+
